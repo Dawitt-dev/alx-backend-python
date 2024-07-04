0cc2af02138b1d183d1e6673ad6611d59a5dc984
“Dawitt-dev” <davoza27@yahoo.com>
2024-07-04 22:35:45 +0000

    task_1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" function concat that takes a string str1 and a string str2"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes a string str1 and a string str2 as
+    arguments and returns a concatenated string
+    """
+    return str1 + str2
+
+
+if __name__ == "__main__":
+    concat()
