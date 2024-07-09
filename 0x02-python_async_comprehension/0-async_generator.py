7f11a0fa9931ee3cc07534abf466e97e22b5635c
“Dawitt-dev” <davoza27@yahoo.com>
2024-07-09 22:39:47 +0000

    task_0. Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -14,14 +14,3 @@ async def async_generator() -> AsyncGenerator[float, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-
-
-if __name__ == "__main__":
-    # This part won't be executed when imported as a module
-    async def print_yielded_values():
-        result = []
-        async for i in async_generator():
-            result.append(i)
-        print(result)
-
-    asyncio.run(print_yielded_values())
