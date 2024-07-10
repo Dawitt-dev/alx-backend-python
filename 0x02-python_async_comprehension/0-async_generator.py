24ce0c6e407ffbd37d613c8174b373e2fa77533b
“Dawitt-dev” <davoza27@yahoo.com>
2024-07-10 13:07:31 +0000

    task

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Async Generator Example"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Async generator that loops 10 times, each
     time asynchronously waits 1 second,
